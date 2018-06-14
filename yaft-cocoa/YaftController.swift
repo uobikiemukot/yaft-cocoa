f6b0f0e094768ff8c25215747f95d086169097f2
uobikiemukot <uobikiemukot@gmail.com>
2018-06-14 23:20:27 +0900

    feature: add new timer

--- yaft-cocoa/YaftController.swift
+++ yaft-cocoa/YaftController.swift
@@ -1,19 +1,20 @@
 import Cocoa
 
 class YaftController: NSViewController {
-    
+
     var yaft: Yaft!
     var yaftView: YaftView!
-    
-    let timer = DispatchSource.makeTimerSource()
+
+    let updateViewTimer = DispatchSource.makeTimerSource()
+    let checkChildTimer = DispatchSource.makeTimerSource()
     var prevEventTimestamp: TimeInterval = 0.0
-    
+
     @IBOutlet weak var imageView: NSImageView!
-    
+
     override func keyDown(with event: NSEvent) {
         yaft.writeToPseudoTerminal(str: event.characters!)
     }
-    
+
     override func rightMouseDown(with event: NSEvent) {
         if prevEventTimestamp == event.timestamp {
             return
@@ -27,7 +28,7 @@ class YaftController: NSViewController {
         }
         prevEventTimestamp = event.timestamp
     }
-    
+
     func addEventMonitors() {
         NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
             self.keyDown(with: $0)
@@ -38,7 +39,7 @@ class YaftController: NSViewController {
             return $0
         }
     }
-    
+
     func updateView() {
         if !yaft.checkPseudoTerminal() {
             return
@@ -49,22 +50,38 @@ class YaftController: NSViewController {
             self.imageView.needsDisplay = true
         }
     }
-    
-    func activateTimer() {
-        timer.schedule(deadline: .now(), repeating: .microseconds(15000), leeway: .microseconds(1000))
-        timer.setEventHandler { self.updateView() }
-        timer.activate()
+
+    func checkChild() {
+        if !yaft.checkChildProcessIsAlive() {
+            DispatchQueue.main.async {
+                if let window = (NSApp.delegate as! AppDelegate).window {
+                    window.performClose(self)
+                }
+            }
+        }
     }
-    
+
+    func activateTimer() {
+        /* check data arrival of PseudoTerminal and update view */
+        updateViewTimer.schedule(deadline: .now(), repeating: .microseconds(15000), leeway: .microseconds(1000))
+        updateViewTimer.setEventHandler { self.updateView() }
+        updateViewTimer.activate()
+
+        /* check child process is alive or not */
+        checkChildTimer.schedule(deadline: .now(), repeating: .seconds(1), leeway: .seconds(1))
+        checkChildTimer.setEventHandler { self.checkChild() }
+        checkChildTimer.activate()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
         addEventMonitors()
-        
+
         // initialize view and model
         yaft = Yaft()
         yaftView = YaftView()
-        
+
         // do something...
         activateTimer()
     }
