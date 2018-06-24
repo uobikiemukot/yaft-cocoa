95c49e2e35f0a0188a58c28ffafea396ac265162
uobikiemukot <uobikiemukot@gmail.com>
2018-06-24 16:13:45 +0900

    style: add preprocessor macro

--- yaft-cocoa/yaft/parse.h
+++ yaft-cocoa/yaft/parse.h
@@ -1,6 +1,8 @@
 /* See LICENSE for licence details. */
-/* parse.h */
-/* char/esc sequence/charset function */
+/* parse.h: char/esc sequence/charset function */
+#ifndef _PARSE_H_
+#define _PARSE_H_
+
 /*
 void control_character(struct terminal_t *term, uint8_t ch);
 void esc_sequence(struct terminal_t *term, uint8_t ch);
@@ -12,3 +14,5 @@ void dcs_sequence(struct terminal_t *term, uint8_t ch);
 void utf8_charset(struct terminal_t *term, uint8_t ch);
 */
 void parse(struct terminal_t *term, uint8_t *buf, size_t size);
+
+#endif /* _PARSE_H_ */
